1481a08ff7662a88e3f56ea6442c252960e91dd3
METO <i@i-meto.com>
2016-08-07 11:07:48 +0800

    读入外挂+最优三分法

--- UVa/UVa 1476 - Error Curves.cpp
+++ UVa/UVa 1476 - Error Curves.cpp
@@ -1,36 +1,38 @@
 #include<bits/stdc++.h>
-#define REP(i,a,b) for(int i=a;i<=b;i++)
-#define FOR(i,a,b) for(int i=a;i<b;i++)
-#define CLR(c,x) memset(c,x,sizeof(c))
-#define sf scanf
-#define pf printf
 #define maxn 10000
-#define eps 1e-10
 using namespace std;
 
 int n;
-struct line{double a,b,c;}Line[maxn+2];
+int a[maxn+2],b[maxn+2],c[maxn+2];
 
 double f(double x){
-	double ans=Line[1].a*x*x+Line[1].b*x+Line[1].c;
-	REP(i,2,n)ans=max(ans,Line[i].a*x*x+Line[i].b*x+Line[i].c);
+	double ans=a[0]*x*x+b[0]*x+c[0];
+	for(int i=1;i!=n;++i)
+		ans=max(ans,a[i]*x*x+b[i]*x+c[i]);
 	return ans;
+}
+
+inline int _int(){
+	int x=0,f=1;
+	char c=getchar();
+	while(c<'0'||c>'9'){if(c=='-')f=-f;c=getchar();}
+	while(c>='0'&&c<='9'){x=x*10+c-'0';c=getchar();}
+	return x*f;
 }
 
 int main(){
 	int T;
-	sf("%d",&T);
+	T=_int();
 	while(T--){
-		sf("%d",&n);
-		REP(i,1,n)sf("%lf%lf%lf",&Line[i].a,&Line[i].b,&Line[i].c);
+		n=_int();
+		for(int i=0;i!=n;++i)a[i]=_int(),b[i]=_int(),c[i]=_int();
 		double L=0,R=1000;
-		while(L+eps<R){
-			double mid1=L+(R-L)/3;
-			double mid2=R-(R-L)/3;
-			if(f(mid1)<f(mid2))R=mid2;
-			else L=mid1;
+		while(L+1e-10<R){
+			double x1=L+(R-L)/2.002,x2=L+R-x1;
+			if(f(x1)<f(x2))R=x2;
+			else L=x1;
 		}
-		pf("%.4f\n",f(L)+eps);
+		printf("%.4f\n",f(L));
 	}
 	return 0;
 }
